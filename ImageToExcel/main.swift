686a33bb1c604feba988df6ce00a673cee768865
Wilsonator5000 <wilsonator5000@gmail.com>
2018-05-05 14:12:42 -0400

    Initial Commit

---
+++ ImageToExcel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ImageToExcel
+//
+//  Created by Wilson Gramer on 5/5/18.
+//  Copyright © 2018 Neef.co. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
